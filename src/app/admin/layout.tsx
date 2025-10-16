a2b5bdfd63e17c3e07863582478163cf197aeef2
Zeeshan Gulzar <xeeshansubs@gmail.com>
2025-10-16 10:33:21 +0500

    [admin session] Add the http basic auth for the admin side.

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,13 +1,13 @@
-import { checkRole } from '@utils/roles';
-import { redirect } from 'next/navigation';
+// import { checkRole } from '@utils/roles';
+// import { redirect } from 'next/navigation';
 
-export default async function AdminLayout({
+export default function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const isAdmin = await checkRole('admin');
-  if (!isAdmin) redirect('/');
+  // const isAdmin = await checkRole('admin');
+  // if (!isAdmin) redirect('/');
 
   return (
     <div className="min-h-screen bg-gray-50">
